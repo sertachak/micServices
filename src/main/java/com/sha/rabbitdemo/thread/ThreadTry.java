5c22faa80711f92ca48eb417e03da852edcaec59
sertachak <sertaconurhakbilen@gmail.com>
2023-12-31 13:07:58 +0300

    Threads volatile

--- src/main/java/com/sha/rabbitdemo/thread/ThreadTry.java
+++ src/main/java/com/sha/rabbitdemo/thread/ThreadTry.java
@@ -16,25 +16,39 @@ public class ThreadTry {
             String threadName = Thread.currentThread().getName();
             System.out.println("ThreadTry.runnable" + threadName);
 
+            int sharedThreadValue = 1;
+
             for (int i = 0; i < 10; i++) {
-                System.out.println("ThreadTry.runnable" + threadName + " " + i);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+                System.out.println("ThreadTry.runnable" + threadName + " " + sharedThreadValue);
+                sharedThreadValue++;
             }
 
         };
-        Thread thread2 = new Thread(runnable);
-        thread2.setDaemon(true);
+
+        Runnable runnableWithASharedValue = new Runnable() { // anonymous class
+
+            private volatile int sharedThreadValue = 1;
+            private volatile boolean sharedThreadValueChanged = false;
+            @Override
+            public void run() {
+                System.out.println("ThreadTry.run"+ " " + Thread.currentThread().getName());
+
+                while(!sharedThreadValueChanged) {
+                    System.out.println("ThreadTry.run" + " " + Thread.currentThread().getName() + " "  + sharedThreadValue);
+                    sharedThreadValue++;
+                    if (sharedThreadValue == 5) {
+                        sharedThreadValueChanged = true;
+                    }
+                }
+            }
+        };
+
+
+        Thread thread2 = new Thread(runnableWithASharedValue);
         thread2.start();
 
-        try {
-            thread2.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        Thread thread3 = new Thread(runnableWithASharedValue);
+        thread3.start();
     }
 }
 
