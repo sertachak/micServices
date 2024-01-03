cc21ec903fb808b33e5481e93461a8d50c3101ef
sertachak <sertaconurhakbilen@gmail.com>
2024-01-03 20:08:44 +0300

    ThreadPool with tryWithSources

--- src/main/java/com/sha/rabbitdemo/thread/ThreadPoolMain.java
+++ src/main/java/com/sha/rabbitdemo/thread/ThreadPoolMain.java
@@ -1,20 +1,42 @@
 package com.sha.rabbitdemo.thread;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class ThreadPoolMain {
 
+
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(ThreadPoolMain.class);
+        try(ClosableExecutorService executorService = new ClosableExecutorService(Executors.newFixedThreadPool(3))) {
+            for(int i = 0; i< 10; i++) {
+                int taskNumber = i;
+                executorService.submit( () -> logger.info("FixedThreadPool" + " " + Thread.currentThread().getName() +  " " + "taskNo: "  + taskNumber));
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+    }
 
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
+    static class ClosableExecutorService implements AutoCloseable {
 
-        for(int i = 0; i< 10; i++) {
-            int taskNumber = i;
-            executorService.submit( () -> System.out.println("FixedThreadPool" + " " + Thread.currentThread().getName() +  " " + "taskNo: "  + taskNumber));
+        private final ExecutorService executorService;
+
+        public ClosableExecutorService(ExecutorService executorService) {
+            this.executorService = executorService;
         }
 
-        executorService.shutdown();
+        @Override
+        public void close() throws Exception {
+            executorService.shutdown();
+        }
+
+        public void submit(Runnable runnable) {
+            executorService.submit(runnable);
+        }
     }
 }
