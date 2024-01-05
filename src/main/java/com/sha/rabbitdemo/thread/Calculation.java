9c1a69d0aeea62171a8a9dc08d6956287d99c6b6
sertachak <sertaconurhakbilen@gmail.com>
2024-01-05 23:33:02 +0300

    Fair lock2

--- src/main/java/com/sha/rabbitdemo/thread/Calculation.java
+++ src/main/java/com/sha/rabbitdemo/thread/Calculation.java
@@ -4,7 +4,7 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Calculation {
 
-    ReentrantLock lock = new ReentrantLock(true);//if parameter is true, then it is a fair lock
+    ReentrantLock lock = new ReentrantLock(true);//if parameter is true, then it is a fair lock this also prevents starvation
 
     private long counter = 0;
 
@@ -57,5 +57,40 @@ public class Calculation {
         public String getType() {
             return type;
         }
+    }
+
+    public static void main(String[] args) {
+        Calculation calculation = new Calculation();
+        System.out.println(calculation.calculate(calculation.new Calculate("ADD"), calculation.new Calculate("ADD"), calculation.new Calculate("SUBTRACT")));
+
+        ReentrantLock lock = new ReentrantLock(true);
+
+        Runnable runnable = () -> {
+            lockTheLockAndSleep(lock, 1000);
+        };
+
+        Thread thread1 = new Thread(runnable, "thread1");
+        Thread thread2 = new Thread(runnable, "thread2");
+        Thread thread3 = new Thread(runnable, "thread3");
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+
+    public static void lockTheLockAndSleep(ReentrantLock lock, long sleepTime) {
+        try {
+            lock.lock();
+            printThreadMessage("sleeping for " + sleepTime + "ms");
+            Thread.sleep(sleepTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void printThreadMessage(String message) {
+        System.out.println(Thread.currentThread().getName() + " " + message);
     }
 }
