096ff66e1b3f99464d68b25a49c61f5c117a0214
sertachak <sertaconurhakbilen@gmail.com>
2024-01-17 21:55:47 +0300

    Lost signal solution

--- src/main/java/com/sha/rabbitdemo/thread/signal/SignalCarrier.java
+++ src/main/java/com/sha/rabbitdemo/thread/signal/SignalCarrier.java
@@ -8,16 +8,29 @@ public class SignalCarrier {
     //wait releases the lock on the object and goes to waiting state
     //notify wakes up one of the waiting threads on the object
 
+    private int threadsWaiting = 0;
+    private int signals = 0;
+
     public void doWait() throws InterruptedException {
         synchronized(this) {
+            if(signals >= 0) {
+                System.out.println(Thread.currentThread().getName() + ": consumed signal at signal count: " + signals);
+                signals--;
+                return;
+            }
+            threadsWaiting++;
             System.out.println(Thread.currentThread().getName() + ": Calling wait");
             this.wait();
+            threadsWaiting--;
             System.out.println(Thread.currentThread().getName() + ": Exited wait");
         }
     }
 
     public void doNotify() {
         synchronized (this) {
+            if(threadsWaiting == 0) {
+                signals++;
+            }
             System.out.println(Thread.currentThread().getName() + ": Calling notify");
             this.notify();
             System.out.println(Thread.currentThread().getName() + ": Notify is called");
