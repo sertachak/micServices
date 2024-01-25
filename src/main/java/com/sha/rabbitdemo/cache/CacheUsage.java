26d14a97c034e20ecde910e219756dcdb18b9696
sertachak <sertaconurhakbilen@gmail.com>
2024-01-25 22:32:25 +0300

    Cache usage

--- src/main/java/com/sha/rabbitdemo/cache/CacheUsage.java
+++ src/main/java/com/sha/rabbitdemo/cache/CacheUsage.java
@@ -6,8 +6,24 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import java.util.concurrent.TimeUnit;
 
 public class CacheUsage {
-    Cache<String, DataObject> cache = Caffeine.newBuilder()
-            .expireAfterWrite(1, TimeUnit.MINUTES)
-            .maximumSize(100)
-            .build();
+
+    public static void main(String[] args) {
+        Cache<String, DataObject> caffeineCache = Caffeine.newBuilder()
+                .expireAfterWrite(1, TimeUnit.MINUTES)
+                .maximumSize(100)
+                .build();
+
+        DataObject dataObject = caffeineCache.getIfPresent("data");
+        if (dataObject == null) {
+            dataObject = caffeineCache.get("data", k -> DataObject.get("data"));
+            System.out.println(caffeineCache.getIfPresent("data").getData());
+        }
+        System.out.println(caffeineCache.getIfPresent("data").getData());
+        caffeineCache.invalidateAll();
+        if (caffeineCache.getIfPresent("data") != null) {
+            System.out.println(caffeineCache.getIfPresent("data").getData());
+        } else {
+            System.out.println("Cache is empty");
+        }
+    }
 }
