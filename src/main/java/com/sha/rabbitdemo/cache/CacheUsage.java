c9026248062d4c2408af4ba7ae9bfad939f71103
sertachak <sertaconurhakbilen@gmail.com>
2024-01-26 22:36:45 +0300

    Async cache and asyncLoadingCache

--- src/main/java/com/sha/rabbitdemo/cache/CacheUsage.java
+++ src/main/java/com/sha/rabbitdemo/cache/CacheUsage.java
@@ -1,8 +1,11 @@
 package com.sha.rabbitdemo.cache;
 
+import com.github.benmanes.caffeine.cache.AsyncCache;
+import com.github.benmanes.caffeine.cache.AsyncLoadingCache;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class CacheUsage {
@@ -25,5 +28,17 @@ public class CacheUsage {
         } else {
             System.out.println("Cache is empty");
         }
+
+        AsyncLoadingCache<String, DataObject> caffeineCacheAsync = Caffeine.newBuilder()
+                .expireAfterWrite(1, TimeUnit.MINUTES)
+                .maximumSize(10)
+                .buildAsync(k -> DataObject.get("Data for : " + k));
+
+        caffeineCacheAsync.get( "key").thenAccept(data -> System.out.println(data.getData()));
+
+        caffeineCacheAsync.get("key1").thenAccept(data -> System.out.println(data.getData()));
     }
+    // build an sync cache without looking to the documentation
+    // build an async cache without looking to the documentation
+
 }
