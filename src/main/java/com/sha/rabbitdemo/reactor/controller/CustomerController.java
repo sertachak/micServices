18422a91f3a005beddd11bade5473af0e2afea57
sertachak <sertaconurhakbilen@gmail.com>
2024-01-30 19:39:51 +0300

    Flux

---
+++ src/main/java/com/sha/rabbitdemo/reactor/controller/CustomerController.java
@@ -0,0 +1,24 @@
+package com.sha.rabbitdemo.reactor.controller;
+
+import com.sha.rabbitdemo.reactor.dao.CustomerDao;
+import com.sha.rabbitdemo.reactor.dto.Customer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    private final CustomerDao customerDao;
+
+    public CustomerController(CustomerDao customerDao) {
+        this.customerDao = customerDao;
+    }
+
+    @GetMapping("/reactive")
+    public Flux<Customer> loadAllCustomers() {
+        return customerDao.loadAllCustomersStream();
+    }
+}
---
+++ src/main/java/com/sha/rabbitdemo/reactor/dto/Customer.java
@@ -0,0 +1,14 @@
+package com.sha.rabbitdemo.reactor.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+        private String id;
+        private String name;
+}
