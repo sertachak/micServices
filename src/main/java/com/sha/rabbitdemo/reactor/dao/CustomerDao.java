b27705f58cc563259665ecda56f3433283bf82c9
sertachak <sertaconurhakbilen@gmail.com>
2024-01-30 19:50:37 +0300

    Added delaying option the the flux emit

--- src/main/java/com/sha/rabbitdemo/reactor/dao/CustomerDao.java
+++ src/main/java/com/sha/rabbitdemo/reactor/dao/CustomerDao.java
@@ -10,16 +10,26 @@ import java.util.stream.IntStream;
 @Component
 public class CustomerDao {
 
+    public void sleepSeconds(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     public List<Customer> loadAllCustomersList() {
         return IntStream.range(1, 50)
                 .peek(i -> System.out.println("Emitting " + i))
+                .peek(i -> sleepSeconds(1))
                 .mapToObj(i -> new Customer(String.valueOf(i), "Object" + i))
                 .toList();
     }
 
     public Flux<Customer> loadAllCustomersStream() {
         return Flux.range(1, 50)
+                .delayElements(java.time.Duration.ofSeconds(1))
                 .doOnEach(i -> System.out.println("Emitting " + i.get()))
                 .map(i -> new Customer(String.valueOf(i), "Object" + i))
                 .doAfterTerminate(() -> System.out.println("Completed"));
