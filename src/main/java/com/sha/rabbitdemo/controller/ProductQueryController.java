4e3fb74e2d1fd897dc76fa28eeee705670d0499f
sertachak <sertaconurhakbilen@gmail.com>
2024-02-21 21:37:19 +0300

    Query for find products

--- src/main/java/com/sha/rabbitdemo/controller/ProductQueryController.java
+++ src/main/java/com/sha/rabbitdemo/controller/ProductQueryController.java
@@ -1,9 +1,14 @@
 package com.sha.rabbitdemo.controller;
 
+import com.sha.rabbitdemo.model.ProductRestModel;
+import com.sha.rabbitdemo.query.FindProductsQuery;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("productQuery/api/v1")
@@ -16,7 +21,9 @@ public class ProductQueryController {
     }
 
     @GetMapping
-    public String getProduct() {
-        return "Get product was called";
+    public List<ProductRestModel> getProduct() {
+        FindProductsQuery findProductsQuery = new FindProductsQuery();
+        final List<ProductRestModel> products = queryGateway.query(findProductsQuery, ResponseTypes.multipleInstancesOf(ProductRestModel.class)).join();
+        return products;
     }
 }
