87d203cb6b9b85b52a649326d525627850e76c66
sertachak <sertaconurhakbilen@gmail.com>
2024-02-26 22:04:05 +0300

    MessageDispatchInterceptor bean

--- src/main/java/com/sha/rabbitdemo/RabbitDemoApplication.java
+++ src/main/java/com/sha/rabbitdemo/RabbitDemoApplication.java
@@ -1,7 +1,12 @@
 package com.sha.rabbitdemo;
 
+import com.sha.rabbitdemo.command.CreateProductCommandInterceptor;
+import org.axonframework.commandhandling.CommandBus;
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
@@ -10,6 +15,11 @@ public class RabbitDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RabbitDemoApplication.class, args);
+    }
+
+    @Autowired
+    public void registerCreateProductCommandMessageDispatchInterceptor(ApplicationContext context, CommandBus commandBus) {
+        commandBus.registerDispatchInterceptor(context.getBean(CreateProductCommandInterceptor.class));
     }
 
 }
